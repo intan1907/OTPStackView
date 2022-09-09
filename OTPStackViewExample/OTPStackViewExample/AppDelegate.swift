53352761e6bf66a85da8df7957c6fa88cde6d1cf
Intan Nurjanah <intan3951@gmail.com>
2022-09-09 17:38:49 +0700

    feat: add IQKeyboardManager to the example project

--- OTPStackViewExample/OTPStackViewExample/AppDelegate.swift
+++ OTPStackViewExample/OTPStackViewExample/AppDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by Intan Nurjanah on 09/09/22.
 //
 
-import UIKit
+import IQKeyboardManager
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.shared().isEnabled = true
         return true
     }
 
